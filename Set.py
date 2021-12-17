eaa0455985c588d6ed203ca45dc8c979f3b396a3
arifcse4 <arif.cse4@gmail.com>
2021-12-18 00:54:57 +0600

    Removing item using discard() method.

--- Set.py
+++ Set.py
@@ -1,12 +1,12 @@
 set1 = set([1,2,3,4,5])
 set2 = set([3,4,5,6,7])
-set3 = set([1,2,3,4,5,6,7])
+set3 = set([1,2,3,4,5,6,7, 8])
 
 print(set1)
 print(set2)
 print(set3)
 
-#Adding items to the set3 using add() method.
-set3.update([8, 9])
+#Remobing items to the set3 using discard() method.
+set3.discard(8)
 
 print(set3)
