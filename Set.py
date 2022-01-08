6ed7d0208f041f39a37d059070b07c1aae48b244
arifcse4 <arif.cse4@gmail.com>
2022-01-08 22:55:34 +0600

    Frozen Set

--- Set.py
+++ Set.py
@@ -1,12 +1,7 @@
-Days1 = {"Monday",  "Tuesday", "Wednesday", "Thursday"}    
-Days2 = {"Monday", "Tuesday"}    
-Days3 = {"Monday", "Tuesday", "Friday"}    
-        
-#Days1 is the superset of Days2 hence it will print true.     
-print (Days1>Days2)     
-        
-#prints false since Days1 is not the subset of Days2     
-print (Days1<Days2)    
-        
-#prints false since Days2 and Days3 are not equivalent     
-print (Days2 == Days3)    
+f = frozenset([1,2,3,4,5])
+
+print(type(f))
+
+for i in f:
+    print(i)
+
