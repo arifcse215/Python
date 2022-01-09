0cae9676adc78ae420bb10a2923c292f8e05d04c
arifcse4 <arif.cse4@gmail.com>
2022-01-09 10:03:54 +0600

    Creating empty dictionary and creating dictionary using dict() method.

--- Dictionary.py
+++ Dictionary.py
@@ -1,2 +1,14 @@
-Dict = {"Name": "Arif", "Dept.": "CSE"}
+# Creating an empty dictionary using curly braces.
+Dict = {}
 print(Dict)
+
+# Creating a dictionary using dict() method
+
+Dict2 = dict({"Name": "Arafat", 'Dept.': 'IP'})
+print(Dict2)
+
+# Creating dictionary with each item as a pair.
+
+Dict3 = dict([('Masum', 'CSE'), ('Arafat', 'IP')])
+
+print(Dict3)
